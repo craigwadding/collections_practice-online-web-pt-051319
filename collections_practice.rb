3afe4e454b5d4ed579a8b66b1fbe8308941cde8a
craig wadding <craig.wadding@gmail.com>
2019-05-19 18:08:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -67,3 +67,33 @@ def add_s (array)
   end
 end
 
+def count_words (words)
+  story_count = Hash.new
+
+  words.split.each do |word|
+    if story_count.include?(word)
+      story_count[word] = story_count[word] + 1
+    else
+      story_count[word] = 1
+    end
+  end
+
+  story_count
+end
+
+
+
+def organize_songs_by_artist (tracks)
+  hash = {}
+  tracks.each do |name|
+    artist = name.split(" - ")[0]
+    trackname = name.split(" - ")[1]
+
+    if hash.include?(artist)
+      hash[artist] << trackname
+    else
+      hash[artist] = [] << trackname
+    end
+  end
+  hash
+end 

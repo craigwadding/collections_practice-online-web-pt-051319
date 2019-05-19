567f60a4e27ab752e188ff2ee3c6c812f0088b4c
craig wadding <craig.wadding@gmail.com>
2019-05-19 18:04:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_arrary_asc (array)
+  array.sort 
+end
+
+def sort_arrary_desc (array)
+  array.sort {|x,y| y <=> x }
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array (array)
+  array.reverse 
+end
+
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end 
+end 
+
+def greater_and_less_than_10 (array)
+  hash = {
+    "greater_than_10" => [],
+    "less_than_10" => []
+  }
+  array.each do |number|
+    if number > 10 
+      hash["greater_than_10"] << number
+    else 
+      hash[less_than_10"] << number 
+    end 
+  end 
+  hash
+end 
 
